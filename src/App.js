3766ce6c36b4ea844b621d31d959824f84856840
Alvito Jordan <alvitojordan12@gmail.com>
2023-10-19 19:04:36 +0700

    add new Route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { Route, Routes } from "react-router-dom";
 import IndonesiaNews from "./pages/Indonesia";
+import CovidNews from "./pages/Covid";
+import ProgrammingNews from "./pages/Programming";
 
 const Router = () => {
   return (
       <Routes>
         <Route path="/" element={<IndonesiaNews />} />
+        <Route path="/covid" element={<CovidNews />} />
+        <Route path="/programming" element={<ProgrammingNews />} />
       </Routes>
   );
 };
