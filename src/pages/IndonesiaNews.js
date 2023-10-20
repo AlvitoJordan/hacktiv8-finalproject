3955e35472d052428b32eed587fa354856ad25ac
RezalMaksmi <rezalnurscc@gmail.com>
2023-10-20 22:02:08 +0700

    menambahkan file textshorter.js dan merapikan tampilan responsive mobile

--- src/pages/IndonesiaNews.js
+++ src/pages/IndonesiaNews.js
@@ -13,7 +13,7 @@ const IndonesiaNews = () => {
 
   useEffect(() => {
     axios
-      .get(`https://newsapi.org/v2/everything?q=indonesia&apiKey=353827dfec9148f8ab42adde79913cd7`)
+      .get(`https://newsapi.org/v2/everything?q=indonesia&apiKey=435f3b9e9edb45b68a597efd2fe796da`)
       .then((response) => {
         setNews(response.data.articles);
         setLoading(false);
@@ -27,41 +27,42 @@ const IndonesiaNews = () => {
   };
 
   return (
-    <div>
-      <h1 className="text-text_color text-5xl font-extrabold w-full text-center border-b-4 border-[#C8CDFF] border-opacity-50 pb-5 mt-10">INDONESIA NEWS</h1>
-
-      <div className="container h-full mx-auto flex justify-center relative items-center flex-col mt-8 mb-8">
-        <div className="flex flex-row flex-wrap justify-center items-start gap-5 w-full mt-6">
-          {loading ? (
-            <div className="flex space-x-5">
-              <Skeleton />
-              <Skeleton />
-              <Skeleton />
-            </div>
-          ) : (
-            <>
-              {currentPageData.map((item, key) => (
-                <CardNew title={item.title} img={item.urlToImage} author={item.author} source={item.source.name} desc={item.description} key={key} />
-              ))}
-            </>
-          )}
+    <div className="bg-bg_color px-[70px] max-[1000px]:px-[20px] h-auto justify-center items-center flex overflow-hidden">
+      <div className="max-w-[1800px] w-full h-full mx-auto flex justify-center relative items-center flex-col mt-8 mb-8">
+        <h1 className="text-text_color text-5xl font-extrabold w-full text-center border-b-4 border-[#C8CDFF] border-opacity-50 pb-5 max-[1000px]:text-3xl">INDONESIA NEWS</h1>
+        <div className="container h-full mx-auto flex justify-center relative items-center flex-col mt-8 mb-8">
+          <div className="flex flex-row flex-wrap justify-center items-start gap-5 w-full mt-6">
+            {loading ? (
+              <div className="flex space-x-5">
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+              </div>
+            ) : (
+              <>
+                {currentPageData.map((item, key) => (
+                  <CardNew title={item.title} img={item.urlToImage} author={item.author} source={item.source.name} desc={item.description} key={key} />
+                ))}
+              </>
+            )}
+          </div>
         </div>
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={2}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination"}
+          activeClassName={"active bg-blue_color border-none text-white_color"}
+          className="flex justify-center space-x-5 mt-6 flex-wrap w-full"
+          previousClassName="flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text_color border-2 border-gray_color"
+          nextClassName="border-2 border-gray_color border-gray-500 flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text_color"
+          pageClassName="border-2 border-gray_color flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text-black mb-4"
+        />
       </div>
-      <ReactPaginate
-        previousLabel={"<"}
-        nextLabel={">"}
-        breakLabel={"..."}
-        pageCount={pageCount}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={2}
-        onPageChange={handlePageChange}
-        containerClassName={"pagination"}
-        activeClassName={"active bg-blue_color border-none text-white_color"}
-        className="flex justify-center space-x-5 mt-6 flex-wrap w-full"
-        previousClassName="flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text_color border-2 border-gray_color"
-        nextClassName="border-2 border-gray_color border-gray-500 flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text_color"
-        pageClassName="border-2 border-gray_color flex justify-center items-center w-[40px] h-[40px] rounded-full font-semibold text-text-black mb-4"
-      />
     </div>
   );
 };
