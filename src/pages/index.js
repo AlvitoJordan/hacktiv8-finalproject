bb70503730e55453ea2ae0406017ecb7075adb11
Alvito Jordan <alvitojordan12@gmail.com>
2023-10-21 21:31:26 +0700

    change import setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import CovidNews from "./CovidNews";
 import IndonesiaNews from "./IndonesiaNews";
 import ProgrammingNews from "./Programming";
+import SearchNews from "./SearchPage";
 
-export { IndonesiaNews, CovidNews, ProgrammingNews };
+export { IndonesiaNews, CovidNews, ProgrammingNews, SearchNews };
