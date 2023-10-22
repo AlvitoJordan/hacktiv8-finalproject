6d64105c79e0a7227838f0fb95b208076cea61a9
RezalMaksmi <rezalnurscc@gmail.com>
2023-10-22 10:27:23 +0700

    menambahkan footer

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,5 +2,7 @@ import { ReactComponent as ICSave } from "./save.svg";
 import { ReactComponent as ICSaveBlue } from "./save-blue.svg";
 import { ReactComponent as ICOpenBar } from "./open-bar.svg";
 import { ReactComponent as ICCloseBar } from "./close-bar.svg";
+import { ReactComponent as ICFooter } from "./hactive8.svg";
 
-export { ICSave, ICOpenBar, ICCloseBar, ICSaveBlue };
+
+export { ICSave, ICOpenBar, ICCloseBar, ICSaveBlue, ICFooter };
